36a3c197817e2683cf754dc6a5d49256c6e0da73
amy0h <amy0h@gmail.com>
2023-09-16 02:25:36 +1200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  const [advice, setAdvice] = React.useState('')
+
+  React.useEffect(function() {
+    fetchData()
+  }, [])
+
+  function fetchData() {
+    fetch('https://api.adviceslip.com/advice')
+      .then(res => res.json())
+      .then(data => setAdvice(data.slip))
+  }
+
+  return (
+    <main>
+      <p className='advice-title'>Advice #{advice.id}</p>
+      <p className='advice-content'>{advice.advice}</p>
+      <img src='images/pattern-divider-mobile.svg' alt='' className='divider mobile' />
+      <img src='images/pattern-divider-desktop.svg' alt='' className='divider desktop' />
+      <div className='button-container'>
+        <button onClick={fetchData}>
+          <img src='images/icon-dice.svg' alt=''/>
+        </button>
+      </div>
+    </main>
+  )
+}
+
+export default App
